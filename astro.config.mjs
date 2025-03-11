0b0b0ea4d77552b6209064ed905c414f5e6b59c1
AndDev <23031429@itcelaya.edu.mx>
2025-03-11 12:24:43 -0600

    Add Vercel adapter configuration with webAnalytics enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,20 @@ import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/blog": "/",
   },
+
   site: "https://example.treelink.com",
   integrations: [tailwind(), sitemap(), icon()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
